151ebb1a51b2fd37ea73928d5dba80649a70b61a
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2020-09-08 18:10:51 +0300

    add hw12

--- script.js
+++ script.js
@@ -8,20 +8,9 @@ let stopButton = document.createElement(`button`);
 body.append(stopButton);
 stopButton.classList.add('stop');
 stopButton.innerHTML = "Stop";
+let timerId;
+let slide;
 
-
-
-// function active(li) {
-//     let active = ul.querySelectorAll('.active');
-//     for (let elem of active) {
-//         elem.classList.remove('active');
-//         document.getElementById(`${elem.classList[1]}`).hidden = true
-//     }
-//     li.classList.add('active');
-//     removeHide(li.classList[1])
-
-// }
-let timer;
 
 function showTimer() {
     let timer = document.createElement('div');
@@ -32,55 +21,68 @@ function showTimer() {
 
 }
 
+
+// $('#test').fadeIn(1000);  
+// $('#test').fadeOut(1000);
+
 function showSlide() {
+    i = 10;
     let image = document.querySelector(".active");
     let nextImage = image.nextElementSibling;
     console.log(nextImage);
     if (nextImage == null) {
         document.querySelector(".image-to-show").classList.add('active');
         nextImage = document.querySelector(".active");
-        i = 10;
-        image.hidden = true;
-        nextImage.removeAttribute('hidden');
+        // image.hidden = true;
+        image.fadeIn(500);
+        // nextImage.removeAttribute('hidden');
+        nextImage.fadeOut(500);
         image.classList.remove('active');
         nextImage.classList.add('active');
-        let timerId = setInterval(() => {
+        timerId = setInterval(() => {
             showTimer();
+            console.log("2")
         }, 1000);
-        setTimeout(() => clearInterval(timerId), 12000);
+        setTimeout(() => clearInterval(timerId), 11000);
     } else {
-        i = 10;
-        image.hidden = true;
-        nextImage.removeAttribute('hidden');
+
+        // image.hidden = true;
+        image.fadeIn(500);
+        // nextImage.removeAttribute('hidden');
+        nextImage.fadeOut(500);
         image.classList.remove('active');
         nextImage.classList.add('active');
-        let timerId = setInterval(() => {
+        timerId = setInterval(() => {
             showTimer();
+            console.log("3")
         }, 1000);
-        setTimeout(() => clearInterval(timerId), 12000);
+        setTimeout(() => clearInterval(timerId), 11000);
     }
 }
-let timerId;
-let slide;
+
 
 let i = 10;
-startButton.addEventListener("click", () => {
 
+startButton.addEventListener("click", () => {
+    i = 10;
+    if (startButton.getAttribute('status') == 'active') {
+        return;
+    }
+    startButton.setAttribute("status", "active");
     timerId = setInterval(() => {
-        showTimer();
-    }, 1000)
+        showTimer(), console.log("1")
+    }, 1000);
+    setTimeout(() => clearInterval(timerId), 11000);
     slide = setInterval(() => {
         showSlide()
-    }, 12000)
-    setTimeout(() => clearInterval(timerId), 12000);
-
-
-
+    }, 11000);
 });
+
+
 stopButton.addEventListener("click", () => {
     clearInterval(slide);
     clearInterval(timerId);
     let timer = document.querySelector('.timer');
     timer.remove();
-    i = 10;
+    startButton.removeAttribute("status");
 });
